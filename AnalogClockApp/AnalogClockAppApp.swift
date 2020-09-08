8560ac4c7f0f1d446fabe00f8a5c2d250057062a
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-09-08 23:13:26 +0300

    Initial Commit

---
+++ AnalogClockApp/AnalogClockAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnalogClockAppApp.swift
+//  AnalogClockApp
+//
+//  Created by Ramill Ibragimov on 08.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct AnalogClockAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnalogClockApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AnalogClockApp
+//
+//  Created by Ramill Ibragimov on 08.09.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
